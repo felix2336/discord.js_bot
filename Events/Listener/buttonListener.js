8cd7bda0e343f4812b7e2d58c5a30f8dc6330366
felix_2336 <110634538+felix2336@users.noreply.github.com>
2023-11-29 13:00:06 +0100

    new

--- Events/Listener/buttonListener.js
+++ Events/Listener/buttonListener.js
@@ -15,12 +15,7 @@ module.exports = {
         
         const button = client.buttons.get(interaction.customId);
         if (!button) return interaction.reply({ content: "Etwas ist schiefgelaufen :/", ephemeral: true });
-        let maintenance;
-        const data = JSON.parse(fs.readFileSync('./maintenance.json', 'utf8'))
-        maintenance = data['maintenance']
-        if (maintenance == true && interaction.user.id == '773072144304963624') return button.execute(interaction, client)
-        if (maintenance == true) return interaction.reply({ content: 'Der Bot befindet sich gerade in Wartungsarbeiten. Daher können keine Buttons verwendet werden.', ephemeral: true })
-
+        
         button.execute(interaction, client);
 
     }
