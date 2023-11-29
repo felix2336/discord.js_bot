6f167c68e8593b525cb546447ed549bbb818571f
felix_2336 <110634538+felix2336@users.noreply.github.com>
2023-11-29 09:21:19 +0100

    added ping.js

---
+++ Commands/General/ping.js
@@ -0,0 +1,14 @@
+const {CommandInteraction} = require('discord.js')
+
+module.exports = {
+	name: 'ping',
+    description: 'Teste, ob der Bot reagiert',
+    permission: 'Sendmessages',
+    
+    /**
+	  *@param {CommandInteraction} interaction
+      */
+
+	async execute(interaction){
+        interaction.reply({content: 'Pong'}) //= interaction.reply('Pong') (content = Nachrichteninhalt)
+    }
